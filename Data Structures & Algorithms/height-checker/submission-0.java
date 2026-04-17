0b9d854325b13d42645ae2fd78f00c2f87e5ad1f
Guruprasad GP <97730899+guruprasadgp22@users.noreply.github.com>
2026-04-17 09:54:12 +0530

    Add: height-checker - submission-0

---
+++ Data Structures & Algorithms/height-checker/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] temp = heights.clone();
+        Arrays.sort(heights);
+        int count = 0;
+
+        for(int i=0;i<heights.length;i++) {
+                System.out.println(temp[i] + " " + heights[i]);
+            if(temp[i] != heights[i]) {
+                count++;
+            }
+        }
+
+        
+
+        return count;
+    }
+}
