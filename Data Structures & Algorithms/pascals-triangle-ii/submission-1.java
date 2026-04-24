d9b959f622e90e84d3c75fa85bb231e39f46bf31
Guruprasad GP <97730899+guruprasadgp22@users.noreply.github.com>
2026-04-24 09:11:09 +0530

    Add: pascals-triangle-ii - submission-1

---
+++ Data Structures & Algorithms/pascals-triangle-ii/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        for(int i=0;i<=rowIndex;i++) {
+            List<Integer> ls = new ArrayList<>();
+
+            for(int j=0;j<=i;j++) {
+                if(j==0 || j==i) {
+                    ls.add(1);
+                } else {
+                    ls.add(list.get(i-1).get(j) + list.get(i-1).get(j-1));
+                }
+            }
+            list.add(ls);
+        }
+
+        return list.get(list.size()-1);
+    }
+}
