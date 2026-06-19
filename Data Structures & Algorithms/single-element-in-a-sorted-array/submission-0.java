d86fc015dbbcb8d4f853a1b1c5532467a40d2b56
Guruprasad GP <97730899+guruprasadgp22@users.noreply.github.com>
2026-06-19 07:54:23 +0530

    Add: single-element-in-a-sorted-array - submission-0

---
+++ Data Structures & Algorithms/single-element-in-a-sorted-array/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int ans = 0;
+
+        for(int ele: nums) {
+            ans ^= ele;
+        }
+
+        return ans;
+    }
+}
