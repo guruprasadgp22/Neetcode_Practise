0109cb3a6b91134c01d6292b95cd8f962dbaaddf
Guruprasad GP <97730899+guruprasadgp22@users.noreply.github.com>
2026-09-01 11:19:57 +0530

    Add: buy-two-chocolates - submission-0

---
+++ Data Structures & Algorithms/buy-two-chocolates/submission-0.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+
+        return money - prices[0] - prices[1] < 0? money: money - prices[0] - prices[1];
+    }
+}
